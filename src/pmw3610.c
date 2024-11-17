21f35ab413876b92c4c00b2161880839abb1c2f3
shakushakupanda <shakushaku4646@gmail.com>
2024-11-17 16:14:21 +0900

    adjustable mouse speed

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -655,6 +655,28 @@ static int pmw3610_report_data(const struct device *dev) {
         y = -y;
     }
 
+    int16_t movement_size = abs(x) + abs(y);
+
+    float speed_multiplier = 1.0; //速度の倍率
+    if (movement_size > 60) {
+        speed_multiplier = 3.0;
+    }else if (movement_size > 30) {
+        speed_multiplier = 1.5;
+    }else if (movement_size > 5) {
+        speed_multiplier = 1.0;
+    }else if (movement_size > 4) {
+        speed_multiplier = 0.9;
+    }else if (movement_size > 3) {
+        speed_multiplier = 0.7;
+    }else if (movement_size > 2) {
+        speed_multiplier = 0.5;
+    }else if (movement_size > 1) {
+        speed_multiplier = 0.2;
+    }
+
+    x = x * speed_multiplier;
+    y = y * speed_multiplier;
+
 #ifdef CONFIG_PMW3610_SMART_ALGORITHM
     int16_t shutter =
         ((int16_t)(buf[PMW3610_SHUTTER_H_POS] & 0x01) << 8) + buf[PMW3610_SHUTTER_L_POS];
