7efddffafeff241461ef07fd539dec310654bcb2
shakushakupanda <shakushaku4646@gmail.com>
2024-11-17 17:28:06 +0900

    Update pmw3610.c

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -637,14 +637,14 @@ static int pmw3610_report_data(const struct device *dev) {
         speed_multiplier = 3.0;
     }else if (movement_size > 30) {
         speed_multiplier = 1.5;
-    }else if (movement_size > 10) {
+    }else if (movement_size > 5) {
         speed_multiplier = 1.0;
-    }else if (movement_size > 8) {
+    }else if (movement_size > 4) {
         speed_multiplier = 0.9;
-    }else if (movement_size > 6) {
-        speed_multiplier = 0.5;
     }else if (movement_size > 3) {
-        speed_multiplier = 0.3;
+        speed_multiplier = 0.4;
+    }else if (movement_size > 2) {
+        speed_multiplier = 0.2;
     }else if (movement_size > 1) {
         speed_multiplier = 0.1;
     }
