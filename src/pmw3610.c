b09035db32c0015e3bb04f35eac93d54d47cfd2f
shakushakupanda <shakushaku4646@gmail.com>
2024-11-17 16:40:19 +0900

    Add files via upload

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -630,6 +630,28 @@ static int pmw3610_report_data(const struct device *dev) {
     int16_t raw_y =
         TOINT16((buf[PMW3610_Y_L_POS] + ((buf[PMW3610_XY_H_POS] & 0x0F) << 8)), 12) / dividor;
 
+    int16_t movement_size = abs(raw_x) + abs(raw_y);
+
+    float speed_multiplier = 1.0; //速度の倍率
+    if (movement_size > 60) {
+        speed_multiplier = 3.0;
+    }else if (movement_size > 30) {
+        speed_multiplier = 1.5;
+    }else if (movement_size > 5) {
+        speed_multiplier = 1.0;
+    }else if (movement_size > 4) {
+        speed_multiplier = 0.9;
+    }else if (movement_size > 3) {
+        speed_multiplier = 0.7;
+    }else if (movement_size > 2) {
+        speed_multiplier = 0.5;
+    }else if (movement_size > 1) {
+        speed_multiplier = 0.2;
+    }
+
+    raw_x = raw_x * speed_multiplier;
+    raw_y = raw_y * speed_multiplier;
+
     int16_t x;
     int16_t y;
 
@@ -654,28 +676,6 @@ static int pmw3610_report_data(const struct device *dev) {
     if (IS_ENABLED(CONFIG_PMW3610_INVERT_Y)) {
         y = -y;
     }
-
-    int16_t movement_size = abs(x) + abs(y);
-
-    float speed_multiplier = 1.0; //速度の倍率
-    if (movement_size > 60) {
-        speed_multiplier = 3.0;
-    }else if (movement_size > 30) {
-        speed_multiplier = 1.5;
-    }else if (movement_size > 5) {
-        speed_multiplier = 1.0;
-    }else if (movement_size > 4) {
-        speed_multiplier = 0.9;
-    }else if (movement_size > 3) {
-        speed_multiplier = 0.7;
-    }else if (movement_size > 2) {
-        speed_multiplier = 0.5;
-    }else if (movement_size > 1) {
-        speed_multiplier = 0.2;
-    }
-
-    x = x * speed_multiplier;
-    y = y * speed_multiplier;
 
 #ifdef CONFIG_PMW3610_SMART_ALGORITHM
     int16_t shutter =
